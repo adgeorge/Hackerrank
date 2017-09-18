593b1b2235903ddc46f2c242b8c85ed02ae43d8f
adgeorge <ad.george17@gmail.com>
2017-09-18 16:53:59 -0700

    Initial set up

---
+++ CrackingTheCodingInterview/Queues/queues_functional_test.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class MyQueue(object):
+    def __init__(self):
+        pass
+    def peek(self):
+        pass
+
+    def pop(self):
+        pass
+    def put(self, value):
+        pass
+
+
+# queue = MyQueue()
+# t = int(input())
+# for line in range(t):
+#     values = map(int, input().split())
+#     values = list(values)
+#     if values[0] == 1:
+#         queue.put(values[1])
+#     elif values[0] == 2:
+#         queue.pop()
+#     else:
+#         print(queue.peek())
+
+
+
+class TestQueues(unittest.TestCase):
+   pass
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+unittest.main()
